fcd05a28386a3517c7d227650c7dee149170fe2f
Katarzyna Kabala <katarzyna_kabala@o2.pl>
2022-02-03 13:36:22 +0100

    BEZ-29 add category list bar, onclick change the displaying businesses

--- src/App.js
+++ src/App.js
@@ -5,30 +5,24 @@ import ProductView from "./views/ProductView";
 import ProfileView from "./views/ProfileView";
 import { useEffect, useState } from "react";
 import { getBusinessList } from "./utils/db";
-
-
-
-
+import { businessListContext } from "./contexts/BusinessListContext";
 
 function App() {
-
-const [businessList, setBusinessList] = useState ([])
-useEffect (() => {
- getBusinessList (setBusinessList)
- console.log(businessList)
-},[])
-  return (
-    <Routes>
-      <Route path="/" element={<HomeView />} />
-      <Route path="/category" element={<CategoryView />} />
-      <Route path="/product" element={<ProductView />} />
-      <Route path="/profile" element={<ProfileView />} />
-    </Routes>
-  );
+	const [businessList, setBusinessList] = useState([]);
+	useEffect(() => {
+		getBusinessList(setBusinessList);
+		console.log(businessList);
+	}, []);
+	return (
+		<businessListContext.Provider value={businessList}>
+			<Routes>
+				<Route path="/" element={<HomeView />} />
+				<Route path="/category" element={<CategoryView />} />
+				<Route path="/product" element={<ProductView />} />
+				<Route path="/profile" element={<ProfileView />} />
+			</Routes>
+		</businessListContext.Provider>
+	);
 }
 
 export default App;
-
-
-
-
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,5 +1,12 @@
-const HomeView = () => {
-  return <div>HomeView</div>;
+import CategoryListBar from "../components/Header/CategoryListBar/CategoryListBar";
+
+const HomeView = ({ businessList }) => {
+	return (
+		<div>
+			HomeView
+			<CategoryListBar businessList={businessList} />
+		</div>
+	);
 };
 
 export default HomeView;
