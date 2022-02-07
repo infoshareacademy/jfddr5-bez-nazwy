c100c29c1dcb8e8ae40c485de72732977f3a53f6
Katarzyna Kabala <katarzyna_kabala@o2.pl>
2022-02-07 22:29:15 +0100

    add basic elements to productView, fix the multiply diplaying calendar bug

--- src/components/CategoryList/ServicesList.js
+++ src/components/CategoryList/ServicesList.js
@@ -1,49 +1,16 @@
-import { useState } from "react";
-import Calendar from "react-calendar";
-
-import s from "./CategoryList.module.css";
+import Service from "./Service";
+// import s from "./CategoryList.module.css";
 
 const ServicesList = ({ business, servicesList }) => {
-	const [displayCalendar, setDisplayCalendar] = useState(false);
-
 	return (
 		<>
 			{servicesList?.map((bus) => (
 				<div key={bus.businessId}>
 					{bus.businessId === business.id
 						? bus.services.map((service) => (
-								<div
-									key={service.id}
-									className={s.servicesList}>
-									<div className={s.servicesListName}>
-										{service.name}
-									</div>
-									<div className={s.servicesListPrice}>
-										{service.price}zł
-									</div>
-									<div className={s.servicesListPrice}>
-										{service.slot}
-									</div>
-									<button
-										className={s.servicesButton}
-										onClick={(id) =>
-											setDisplayCalendar(!displayCalendar)
-										}>
-										Zarezerwuj
-									</button>
-								</div>
+								<Service service={service} />
 						  ))
 						: null}
-
-					{displayCalendar && (
-						<div>
-							<Calendar
-								onClickDay={(value, event) =>
-									console.log(value)
-								}
-							/>
-						</div>
-					)}
 				</div>
 			))}
 		</>
