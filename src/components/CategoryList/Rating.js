532630d158a32511a7f01190ee4336c8974acc2b
maria Drozdowska <drozdowska.maria1@gmail.com>
2022-02-10 17:30:47 +0100

    styling opinons

--- src/components/CategoryList/Rating.js
+++ src/components/CategoryList/Rating.js
@@ -6,16 +6,23 @@ const Rating = ({ rating }) => {
 	const ratingArray = Array.from(Array(rating.value).keys());
 
 	return (
-		<div>
-			<h3>{rating.user}</h3>
-			<div>
-				<div>
-					{stars.map(() => (
-						<img src={star} className={styles.star} />
-					))}
+		<div className={styles.comment}>
+			<div className={styles.opinionsHeader}>
+				<h3>{rating.user}</h3>
+				<div className={styles.starsContainer}>
+					<div className={styles.starWrapper}>
+						{stars.map(() => (
+							<img src={star} className={styles.star} />
+						))}
+					</div>
+					<div className={styles.ratedStars}>
+						{ratingArray.map(() => (
+							<img src={star} className={styles.star} />
+						))}
+					</div>
 				</div>
-				{rating.value} {rating.comment}
 			</div>
+			{rating.comment}
 		</div>
 	);
 };
