61366f2f8c8c8a22dbc464c0e485ad11790e8540
Emilia Brzezińska <94071705+emiliabrzezinska@users.noreply.github.com>
2022-02-11 00:29:01 +0100

    Footer finished

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,57 +4,68 @@ import githublogo from "./images/github.png";
 const Footer = () => {
 	return (
 		<>
-		<div style={{height: "1000px"}}></div>
-		<div className={styles.container}>
-			<div className={styles.left}>
-				<div>
-					<div className={styles.box}>
-						<div className={styles.mark}>petsy</div>
-						<div className={styles.aboutus}>
-						
+			<div style={{ height: "1000px" }}></div>
+			<div className={styles.container}>
+				<div className={styles.left}>
+					<div>
+						<div className={styles.box}>
+							<div className={styles.mark}>petsy</div>
+							<div className={styles.belt}></div>
+						</div>
+						<div className={styles.rights}>
+							<a href=""></a>@ 2022 petsy Inc. Wszystkie prawa
+							zastrzeżone
 						</div>
 					</div>
-					<div className={styles.rights}>
-						<a href=""></a>@ 2022 petsy Inc. Wszystkie prawa
-						zastrzeżone
+					<div className={styles.info}>
+						<div className={styles.contact}>KONTAKT</div>
+						<div>ul. FrontEndowska 5</div>
+						<div>e-mail: najlepszi.developerzy@gmail.com</div>
+						<div>00-600 DevCity</div>
 					</div>
 				</div>
-				<div className={styles.info}>
-					<div className={styles.contact}>Kontakt</div>
-					<div>ul. FrontEndowska 1</div>
-					<div>e-mail: najlepszideveloperzy@gmail.com</div>
-					<div>00-600 DevCity</div>
-				</div>
-			</div>
 
-			<div className={styles.right}>
-				<div className={styles.creators}>
-					<div className={styles.created}>Created by:</div>
-					<div className={styles.github}>
-						<span className={styles.author}>Marysia</span>
-						<a href="https://github.com/drozdowska-maria" target="blank">
-							<img src={githublogo}></img>
-						</a>
-						<span className={styles.author}>Kasia</span>
-						<a href="">
-							<img src={githublogo}></img>
-						</a>
-						<span className={styles.author}>Daniel</span>
-						<a href="">
-							<img src={githublogo}></img>
-						</a>
-						<span className={styles.author}>Asia</span>
-						<a href="">
-							<img src={githublogo}></img>
-						</a>
-						<span className={styles.author}>Emilka</span>
-						<a href="">
-							<img src={githublogo}></img>
-						</a>
+				<div className={styles.right}>
+					<div className={styles.creators}>
+						<div className={styles.created}>TWÓRCY APLIKACJI:</div>
+						<div className={styles.github}>
+							<span className={styles.author}>
+								Maria<br></br>Drozdowska
+							</span>
+							<a
+								href="https://github.com/drozdowska-maria"
+								target="blank">
+								<img src={githublogo}></img>
+							</a>
+
+							<span className={styles.author}>
+								Katarzyna<br></br> Kabała
+							</span>
+							<a href="">
+								<img src={githublogo}></img>
+							</a>
+							<span className={styles.author}>
+								Daniel<br></br> Jurkiewicz
+							</span>
+							<a href="">
+								<img src={githublogo}></img>
+							</a>
+							<span className={styles.author}>
+								Joanna <br></br>Florczak
+							</span>
+							<a href="">
+								<img src={githublogo}></img>
+							</a>
+							<span className={styles.author}>
+								Emilia <br></br>Brzezińska
+							</span>
+							<a href="https://github.com/emiliabrzezinska">
+								<img src={githublogo}></img>
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 		</>
 	);
 };
