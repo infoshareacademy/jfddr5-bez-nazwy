58ee15f3b61b9050e3f57b0af1f1627ed808a9a3
Joanna Florczak <zdziha1000@gmail.com>
2022-02-11 10:47:28 +0100

    small changes in styling

--- src/PageElements.js
+++ src/PageElements.js
@@ -7,23 +7,28 @@ const PageElements = () => {
 
          return (
             <div className={s.divStyle}> 
-                <h2>Umów swojego Futrzaka na wizytę 
+                <div className={s}>
+                <h2>Umów swojego Futrzaka na wizytę</h2>
                     <p>
                         Chcesz umówić swojego pupila do groomera,hodowcy, weterynarza,behawiorysty lub hotelu dla zwierząt w swojej okolicy?Szukasz miejsca, w tórym twoja pociecha otrzyma najlepszą opiekę pod słońcem?
                         Petsy to darmowa aplikacja do rezerwacji, dzięki swojej łatwości w obsłudze ,bez poroblemu znajdziesz dogodny termin dla siebeie i swojego pupila. Bez wychodzenia z domu albo sięgania po telefon-na petsy zarezerwujesz wygodny dla was termin o każdej porze bezwzględu na miejsce.
                         <strong>Odkrywaj nowe usługi dla swoich Milusińskich i umawiaj ich na wizyty z petsy </strong>
-                        <img src={pets1} alt="four dogs looking at cookie in hand" className={s.imgStyle}/>
+                        
                     </p>
-                </h2>
-                <h2> Specjaliści w Twojej okolicy
+                </div>
+                <div><img src={pets1} alt="four dogs looking at cookie in hand" className={s.imgStyle}/></div>
+                
+                <div>
+                <h2> Specjaliści w Twojej okolicy </h2>
                     <p>
                         W petsy znajdziesz najlepsze salony dla swoich Ulubieńców w wygodnej dla was okolicy.
                         Dowiedz się o nich jak najwięcej - przeczytaj opinie innych klientów i przejrzyj usługi jakie oferują.
                         Oszczędź czas na umawianiu wizyt. Dzięki petsy będziesz miał więcej czasu na zabawę ze swoim Futrzakiem.
                         <strong>Nasza aplikacja jest bardzo prosta w użyciu</strong>
-                        <img src={pets2} alt="cat touching hand with woman" className={s.imgStyle}/>
+                        
                     </p>
-                </h2>
+                </div>
+                <div><img src={pets2} alt="cat touching hand with woman" className={s.imgStyle}/></div>
             </div>
          )
      
