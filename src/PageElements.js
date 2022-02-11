917a7e7616d67f39c21829a6377cc85d5ab7da2c
Joanna Florczak <zdziha1000@gmail.com>
2022-02-11 11:00:03 +0100

    padding deleated

--- src/PageElements.js
+++ src/PageElements.js
@@ -7,7 +7,7 @@ const PageElements = () => {
 
          return (
             <div className={s.divStyle}> 
-                <div className={s}>
+                <div className={s.textBox}>
                 <h2>Umów swojego Futrzaka na wizytę</h2>
                     <p>
                         Chcesz umówić swojego pupila do groomera,hodowcy, weterynarza,behawiorysty lub hotelu dla zwierząt w swojej okolicy?Szukasz miejsca, w tórym twoja pociecha otrzyma najlepszą opiekę pod słońcem?
@@ -19,7 +19,7 @@ const PageElements = () => {
                 <div><img src={pets1} alt="four dogs looking at cookie in hand" className={s.imgStyle}/></div>
                 
                 <div>
-                <h2> Specjaliści w Twojej okolicy </h2>
+                <h2 className={s.textBox2}>Specjaliści w Twojej okolicy</h2>
                     <p>
                         W petsy znajdziesz najlepsze salony dla swoich Ulubieńców w wygodnej dla was okolicy.
                         Dowiedz się o nich jak najwięcej - przeczytaj opinie innych klientów i przejrzyj usługi jakie oferują.
